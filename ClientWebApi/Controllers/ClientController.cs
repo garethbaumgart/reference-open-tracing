c74f0ebcbda4c8f6b400f17cb92f8a9c708a6434
Gareth Baumgart <gareth.baumgart@wexinc.com>
2021-12-20 15:29:06 +1100

    add initial docker-compose and client proj

---
+++ ClientWebApi/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ClientWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ClientController
+    {
+        HttpClient _httpClient;
+        public ClientController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://localhost:5000/api");
+        }
+
+        [HttpGet(Name = "GetValuesFromServer")]
+        public async Task<IEnumerable<string>> Get()
+        {
+            var response = await _httpClient.GetStringAsync("server");
+            return JsonSerializer.Deserialize<IEnumerable<string>>(response) ?? Enumerable.Empty<string>();
+        }
+    }
+}
