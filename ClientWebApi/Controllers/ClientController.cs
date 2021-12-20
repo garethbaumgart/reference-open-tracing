a57081f49dad156cb3f66b334a5b42613f169ed7
Gareth Baumgart <gareth.baumgart@wexinc.com>
2021-12-20 16:07:55 +1100

    added serverwebapi

--- ClientWebApi/Controllers/ClientController.cs
+++ ClientWebApi/Controllers/ClientController.cs
@@ -5,20 +5,35 @@ namespace ClientWebApi.Controllers
 {
     [ApiController]
     [Route("api/[Controller]")]
-    public class ClientController
+    public class ClientController : ControllerBase
     {
         HttpClient _httpClient;
         public ClientController(HttpClient httpClient)
         {
             _httpClient = httpClient;
-            _httpClient.BaseAddress = new Uri("http://localhost:5000/api");
+            _httpClient.BaseAddress = new Uri("http://serverwebapi/api/");
         }
 
         [HttpGet(Name = "GetValuesFromServer")]
-        public async Task<IEnumerable<string>> Get()
+        public async Task<ActionResult<IEnumerable<string>>> Get()
         {
-            var response = await _httpClient.GetStringAsync("server");
-            return JsonSerializer.Deserialize<IEnumerable<string>>(response) ?? Enumerable.Empty<string>();
+            try
+            {
+                var response = await _httpClient.GetStringAsync("server");
+                var values = JsonSerializer.Deserialize<IEnumerable<string>>(response) ?? Enumerable.Empty<string>();
+                throw new Exception("boom");
+                return Ok(values);
+            }
+            catch (Exception ex)
+            {
+                // todo add to span log
+                return new ObjectResult(
+                new
+                {
+                    Value = ex.Message,
+                    StatusCode = 500
+                });
+            }
         }
     }
 }
---
+++ ServerWebApi/Controllers/ServerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ServerController
+    {
+        [HttpGet(Name = "GetValuesFromDatabase")]
+        public Task<IEnumerable<string>> Get()
+        {
+            var tempValues = new List<string>() { "Value One", "Value Two" };
+            return Task.FromResult(tempValues.AsEnumerable<string>());
+        }
+    }
+}
